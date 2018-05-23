cc0eebd2a41a0e40866116515993aff26afd501f
Natasha Cox <natashac1014@gmail.com>
2018-05-23 11:40:25 -0500

    it worked

---
+++ main.js
@@ -0,0 +1,10 @@
+let bandNumber =0;
+
+function nameAndNumber (bandName) {
+    bandNumber ++;
+    console.log(`${bandNumber} ${bandName}`)
+    return bandNumber;
+    }
+nameAndNumber("Scum")
+nameAndNumber("Hootie")
+nameAndNumber("Aerosmith")
